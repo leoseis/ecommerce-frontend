75fe47dc671a1682a6eed1f8f5e3206da075a9d6
leoseis <eboscrib2@yahoo.com>
2025-07-19 18:55:41 +0100

    added the NotFoundPage componenet and routing :

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import MainLayout from "./layout/MainLayout"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from "./components/home/HomePage.JSX"
+import NotFoundPage from "./components/ui/NotFoundPage"
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<MainLayout />}>
     <Route index element={<HomePage />} />
+     <Route path="*" element={<NotFoundPage />} />
     </Route>
     </Routes>
 
