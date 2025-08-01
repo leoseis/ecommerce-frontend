ca329a13555601d24a9c7fcb56eed6198f8e3518
leoseis <eboscrib2@yahoo.com>
2025-08-01 15:37:01 +0100

    appended the img url with backend img ur

--- src/components/home/HomeCard.jsx
+++ src/components/home/HomeCard.jsx
@@ -1,13 +1,20 @@
 import styles from "./HomeCard.module.css";
 import { Link } from "react-router-dom";
 
-const HomeCard = ({product}) => {
+import { BASE_URL } from "../../api"; // adjust the path based on your structure
+
+
+// const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:8001";
+
+const HomeCard = ({ product }) => {
   return (
     <div className={`col-md-3 ${styles.col}`}>
-      <Link to="/products/slug">
+      <Link to={`/products/${product.slug}`}>
         <div className={styles.card}>
           <div className={styles.cardImgWrapper}>
-            <img src="" className={styles.cardImgTop} alt="Product Image" />
+            <img src={`${BASE_URL}${product.image}`} 
+            className={styles.cardImgTop}
+            alt={product.name} />
           </div>
           <div className={styles.cardBody}>
             <h5 className={`${styles.cardTitle} mb-1`}>{product.name}</h5>
