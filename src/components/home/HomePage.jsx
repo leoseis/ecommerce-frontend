787f1eb967eb61a218e86a5f95f8610f4859f2c0
leoseis <eboscrib2@yahoo.com>
2025-08-05 05:27:50 +0100

    added a loading state

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -2,19 +2,26 @@ import Header from './Header'
 import CardContainer from './CardContainer'
 import { useEffect, useState } from 'react'
 import api from '../../api'
+import PlaceHolderContainer from '../ui/PlaceHolderContainerr'
 
 const HomePage = () => {
 
   const [products , setProducts] = useState([])
+  const[loading, setLoading] = useState(false)
   useEffect(function(){
-
+  setLoading(true)
   api.get("products")
+
   .then(res =>{
     console.log(res.data)
     setProducts(res.data)
+    setLoading(false)
   })
-
+     .catch(err => {
+    console.log(err.message)
+    setLoading(false)
     
+  })
   },[] );
 
 
@@ -23,6 +30,7 @@ const HomePage = () => {
     <>
     <Header />
     <CardContainer products={products}/>
+    <PlaceHolderContainer/>
     </>
   )
 }
